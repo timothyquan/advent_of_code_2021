9bd0cf54189a33ebc9c6eb283733a8210995b0b5
timothyquan <48138393+timothyquan@users.noreply.github.com>
2021-12-16 11:47:12 +0700

    day_6: part 2 complete.
    Optimized

--- day_06/lanternfish.py
+++ day_06/lanternfish.py
@@ -1,52 +1,28 @@
 from aocd.models import Puzzle
 # https://pypi.org/project/advent-of-code-data/
 
-
-class Fish_School:
-    def __init__(self):
-        self.fishes = []
-        pass
-
-    def add_fish(self, fish):
-        self.fishes.append(fish)
-
-    def print(self):
-        fish_list = []
-        for idx, f in enumerate(self.fishes):
-            fish_list.append(f.age)
-        print(fish_list)
-
-    def age_day(self):
-        for fish in self.fishes:
-            fish.age_day()
-
-
-class Lantern_Fish:
-    def __init__(self, school, age=9):
-        self.age = age
-        self.school = school
-        pass
-
-    def age_day(self):
-        self.age -= 1
-        if self.age < 0:
-            self.school.add_fish(Lantern_Fish(self.school))
-            self.age = 6
-
-
 if __name__ == "__main__":
-    input = '3,4,3,1,2'
-    #input = Puzzle(2021, 6).input_data
+    input = Puzzle(2021, 6).input_data
     ages = input.split(',')
     ages = list(map(int, ages))
-    school = Fish_School()
+    ages = [age+1 for age in ages]
+
+    fishes = [0 for f in range(9)]
     for a in ages:
-        school.add_fish(Lantern_Fish(school, age=a))
+        fishes[a] += 1
 
-    for i in range(80):
-        # print(f"Day: {i}")
-        # school.print()
-        school.age_day()
+    for idx, fish in enumerate(fishes):
+        print(f'{idx}:{fish}')
+    print(f" There are {sum(fishes)} little fishies to start.")
 
-    print(f'There are now {len(school.fishes)} little fishies. ')
-    pass
+    for i in range(257):
+        new_fishes = [0 for f in range(9)]
+        for idx, f in enumerate(fishes):
+            if idx < 1:
+                new_fishes[8] += f
+                new_fishes[6] += f
+            else:
+                new_fishes[idx-1] += f
+        fishes = new_fishes.copy()
+
+    print(f" There are now {sum(fishes)} little fishies.")
