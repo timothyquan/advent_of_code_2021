fb6a2558adeaab094e1914713a5d3927ff2386de
timothyquan <48138393+timothyquan@users.noreply.github.com>
2021-12-27 16:49:14 +0700

    day_10: part 2 complete

--- day_10/syntax_scoring.py
+++ day_10/syntax_scoring.py
@@ -4,6 +4,7 @@ from aocd.models import Puzzle
 class Line:
     def __init__(self, line):
         self.line = list(line)
+        self.chunks = []
         pass
 
     def find_errors(self):
@@ -11,21 +12,31 @@ class Line:
         openers = {'(': ')', '[': ']', '{': '}', '<': '>'}
         closers = {')': '(', ']': '[', '}': '{', '>': '<'}
         err_map = {')': 3, ']': 57, '}': 1197, '>': 25137}
-        chunks = []
+
         print('')
         print(f"Line: {''.join(self.line)}", end='')
         for c in self.line:
             if c in openers:
-                chunks.append(c)
+                self.chunks.append(c)
             elif c in closers.keys():
-                if not closers[c] == chunks[-1]:
+                if not closers[c] == self.chunks[-1]:
                     print(
-                        f":-=-=-=-=-=-=-=-=-=-=-=- Expected {openers[chunks[-1]]} but found {c} instead. ")
+                        f":-=-=-=-=-=-=-=-=-=-=-=- Expected {openers[self.chunks[-1]]} but found {c} instead. ")
                     return err_map[c]
                 else:
-                    chunks.pop(-1)
+                    self.chunks.pop(-1)
         print(":-=-=-=-=-=-=-=-=-=-=-=- No errors detected!")
         return 0
+
+    def find_closure(self):
+        score = 0
+        closers = []
+        points = {'(': 1, '[': 2, '{': 3, '<': 4}
+
+        for c in reversed(self.chunks):
+            score = (score * 5) + points[c]
+            print(c, points[c], end=',')
+        return score
 
 
 if __name__ == "__main__":
@@ -34,7 +45,21 @@ if __name__ == "__main__":
     lines = Puzzle(2021, 10).input_data
 
     errsum = 0
+    line_list = []
     for l in lines.split('\n'):
-        errsum += Line(l).find_errors()
+        line_list.append(Line(l))
+        err = line_list[-1].find_errors()
+        errsum += err
+        if err:
+            line_list.pop(-1)
 
-    print(errsum)
+    print(f"The sum of all first errors is: {errsum}.")
+
+    closcores = []
+    for l in line_list:
+        closcore = l.find_closure()
+        closcores.append(closcore)
+        print(f"{''.join(l.line)} found closure with a score of : {closcore}")
+
+    mid_score = sorted(closcores)[int(len(closcores)/2)]
+    print(f"The middle score is {mid_score}.")
